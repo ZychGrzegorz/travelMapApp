57de34deaeedd1cbf8bec32084316ec1c5e83a8a
Zych Grzegorz <gzych@kbj.com.pl>
2021-09-29 15:52:04 +0200

    simple server created

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const app = express()
+const pinRoute = require('./routes/pins')
+
+dotenv.config()
+
+//allows to use response as json
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('mongoDB connected');
+    })
+    .catch((err) => {
+        console.log(err);
+        console.log('DB not connected');
+    });
+
+
+app.use('/api/pins', pinRoute)
+
+app.listen(8800, () => {
+    console.log('Backend server is running on port 8800');
+})
